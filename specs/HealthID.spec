669c806bd79cb7741317cb9e8eac8d9111644a9c
meghna <meghna.tanwal@thoughworks.com>
2021-06-03 13:45:19 +0530

    Completes doctor prescription with associated healthId

--- specs/HealthID.spec
+++ specs/HealthID.spec
@@ -2,4 +2,18 @@
 
 ## Verify health ID
 * Login to Bahmni as a receptionist
-* Create a new patient with health id "cancerpatient@sbx"
+* Create a new patient with health id "cancerpatient@sbx" with mobile number "+91-9876543210"
+* Open newly created patient details by search
+* Start an OPD
+* Doctor opens the consultation notes "Consultation Notes" for newly created patient "First" "Last"
+* Doctor must be able to prescribe tests
+|TestName|
+|Haemogram|
+|Serum|
+|Liver Function - General|
+|LDH|
+* Goto Bahmni home
+* Open Patient Documents
+* Choose patient "First" "Last"
+* Add a lab report
+* Create a consent request for the healthID "cancerpatient@sbx"
--- tests/frontEndDesk.js
+++ tests/frontEndDesk.js
@@ -30,8 +30,9 @@ const headless = process.env.headless_chrome.toLowerCase() === 'true';
 
 beforeSuite(async () => {
     await openBrowser({
-        headless:headless,
-        args: ["--start-fullscreen"]})
+        headless: headless,
+        args: ["--start-fullscreen"]
+    })
 });
 
 afterSuite(async () => {
@@ -49,33 +50,33 @@ gauge.customScreenshotWriter = async function () {
     return path.basename(screenshotFilePath);
 };
 
-step("Open registration module", async function() {
-        await highlight("Clinical")
-        await click("Registration",toLeftOf("Programs"));
-    });
-    
-    step("To Associate a healthID, vefiy it", async function() {
-        await click("Verify Health ID");
-    });
-    
-    step("Enter healthID <patientHealthID> details", async function (patientHealthID) {
-        await click(textBox(toRightOf("Enter Health ID")));
-        await write(patientHealthID);
-    });
-    
-    step("Fetch authentication modes", async function() {
-        //https://mixedanalytics.com/knowledge-base/api-connector-encode-credentials-to-base-64/
-        const token = process.env.receptionist         
-      
-        await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/v0.5/hip/fetch-modes", (request) => {
-        var body1 ={
+step("Open registration module", async function () {
+    await highlight("Clinical")
+    await click("Registration", toLeftOf("Programs"));
+});
+
+step("To Associate a healthID, vefiy it", async function () {
+    await click("Verify Health ID");
+});
+
+step("Enter healthID <patientHealthID> details", async function (patientHealthID) {
+    await click(textBox(toRightOf("Enter Health ID")));
+    await write(patientHealthID);
+});
+
+step("Fetch authentication modes", async function () {
+    //https://mixedanalytics.com/knowledge-base/api-connector-encode-credentials-to-base-64/
+    const token = process.env.receptionist
+
+    await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/v0.5/hip/fetch-modes", (request) => {
+        var body1 = {
             "authModes": [
                 "MOBILE_OTP",
                 "AADHAAR_OTP"
             ]
         };
         var reqBodyOnFetchModes = JSON.stringify(body1);
-        request.respond({
+        request.respond({
             method: 'POST',
             port: '9052',
             hostname: 'https://ndhm-dev.bahmni-covid19.in/',
@@ -89,20 +90,20 @@ step("Open registration module", async function() {
             }
         })
     })
-                
-        await click(text("Verify",within($(".verify-health-id"))));
+
+    await click(text("Verify", within($(".verify-health-id"))));
 });
 
-step("Select Mobile OTP", async function() {
-    await dropDown("Preferred mode of Authentication").select("MOBILE_OTP");
+step("Select Mobile OTP", async function () {
+    await dropDown("Preferred mode of Authentication").select("MOBILE_OTP");
 });
 
-step("Authenticate with Mobile", async function() {
-    const token = process.env.receptionist         
+step("Authenticate with Mobile", async function () {
+    const token = process.env.receptionist
     var reqBodyOnFetchModes = JSON.stringify("");
 
-    await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/v0.5/hip/auth/init", (request) => {
-        request.respond({
+    await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/v0.5/hip/auth/init", (request) => {
+        request.respond({
             method: 'POST',
             port: '9052',
             hostname: 'https://ndhm-dev.bahmni-covid19.in/',
@@ -119,90 +120,90 @@ step("Authenticate with Mobile", async function() {
     await click(button("Authenticate"))
 });
 
-step("Login as a receptionist with username <userName> password <password> location <location>", async function(userName, password, location) {
-        await write(userName,into(textBox(toRightOf("Username *"))));
-        await write(password,into(textBox(toRightOf("Password *"))));
-        await dropDown("Location").select(location);
-        await click(button("Login"));
+step("Login as a receptionist with username <userName> password <password> location <location>", async function (userName, password, location) {
+    await write(userName, into(textBox(toRightOf("Username *"))));
+    await write(password, into(textBox(toRightOf("Password *"))));
+    await dropDown("Location").select(location);
+    await click(button("Login"));
 });
 
-step("Goto Bahmni home", async function() {
-        await goto(process.env.bahmniHome);
+step("Goto Bahmni home", async function () {
+    await goto(process.env.bahmniHome);
 });
 
-step("Create a new patient with verfication id", async function() {
-    await click("Create New");
+step("Create a new patient with verfication id", async function () {
+    await click("Create New");
 });
 
-step("Enter patient first name <firstName>", async function(firstName) {
-    await write(firstName,into(textBox(toRightOf("Patient Name*"))));
+step("Enter patient first name <firstName>", async function (firstName) {
+    await write(firstName, into(textBox(toRightOf("Patient Name*"))));
 });
 
-step("Enter patient middle name <middleName>", async function(middleName) {
-    await write(middleName,into(textBox({"placeholder" : "Middle Name"})));
+step("Enter patient middle name <middleName>", async function (middleName) {
+    await write(middleName, into(textBox({ "placeholder": "Middle Name" })));
 });
 
-step("Enter patient last name <lastName>", async function(lastName) {
-    await write(lastName,into(textBox({"placeholder" : "Last Name"})));
+step("Enter patient last name <lastName>", async function (lastName) {
+    await write(lastName, into(textBox({ "placeholder": "Last Name" })));
 });
 
-step("Enter patient gender <gender>", async function(gender) {
-	await dropDown("Gender *").select(gender);
+step("Enter patient gender <gender>", async function (gender) {
+    await dropDown("Gender *").select(gender);
 });
 
-step("Enter age of the patient <age>", async function(age) {
+step("Enter age of the patient <age>", async function (age) {
     await write(age, into(textBox(toRightOf("Years"))));
     await click(checkBox(toLeftOf("Estimated")));
 });
 
-step("Enter patient mobile number <mobile>", async function(mobile) {
+step("Enter patient mobile number <mobile>", async function (mobile) {
     await write(mobile, into(textBox(toRightOf("Primary Contact"))));
 });
 
-step("Save the patient data", async function() {
+step("Save the patient data", async function () {
     await click("Save");
     var patientIdentifierValue = await $('#patientIdentifierValue').text();
-    gauge.dataStore.scenarioStore.put("patientIdentifier",patientIdentifierValue);
+    gauge.dataStore.scenarioStore.put("patientIdentifier", patientIdentifierValue);
 });
 
-step("Click Start OPD Visit", async function() {
+step("Click Start OPD Visit", async function () {
     await click("Start OPD Visit");
 });
 
-step("Enter registration fees <arg0>", async function(arg0) {
+step("Enter registration fees <arg0>", async function (arg0) {
     await write("100", into(textBox(toRightOf("Registration Fees"))));
 });
 
-step("Click Save", async function() {
+step("Click Save", async function () {
     await click("Save");
 });
 
-step("Go back to home page", async function() {
+step("Go back to home page", async function () {
     goto("https://ndhm-dev.bahmni-covid19.in/bahmni/home/index.html#/dashboard")
     //    await click($('.back-btn'));
 });
 
-step("Click create new patient", async function() {
-	await click("Create New")
+step("Click create new patient", async function () {
+    await click("Create New")
 });
 
 step("Open newly created patient details by search", async function () {
-	var patientIdentifierValue = gauge.dataStore.scenarioStore.get("patientIdentifier");
+    var patientIdentifierValue = gauge.dataStore.scenarioStore.get("patientIdentifier");
 
     await goto("https://ndhm-dev.bahmni-covid19.in/bahmni/registration/index.html#/search")
-    await write(patientIdentifierValue,into(textBox({"placeholder" : "Enter ID"})))
-    await click("Search",toRightOf(patientIdentifierValue));
+    await write(patientIdentifierValue, into(textBox({ "placeholder": "Enter ID" })))
+    await click("Search", toRightOf(patientIdentifierValue));
 });
 
 step("Verify if healthId <healthID> already exists", async function (healthID) {
-    const token = process.env.receptionist         
-      
-    await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/existingPatients/"+healthID, (request) => {
-    var body1 ={
-        "error":{"code":"PATIENT_ID_NOT_FOUND","message":"No patient found"}
-    };
-    var reqBodyOnFetchModes = JSON.stringify(body1);
-        request.respond({
+    const token = process.env.receptionist
+
+    await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/existingPatients/" + healthID, (request) => {
+        var body1 = {
+            "error": { "code": "PATIENT_ID_NOT_FOUND", "message": "No patient found" }
+        };
+        var reqBodyOnFetchModes = JSON.stringify(body1);
+        request.respond({
             method: 'POST',
             port: '9052',
             body: body1,
@@ -213,42 +214,62 @@ step("Verify if healthId <healthID> already exists", async function (healthID) {
             }
         })
     })
-	
+
 });
 
-step("waitFor <time>", async function(time) {
+step("waitFor <time>", async function (time) {
     await waitFor(time)
 });
 
-step("Enter OTP for health care validation <otp> for healthID <healthID>", 
-async function (otp, healthID) {
-    await write(otp,into(textBox(above("Confirm"))));
-    var patientDetails = JSON.parse(_fileExtension.parseContent("./data/confirm/"+healthID+".json"));
-    const token = process.env.receptionist         
+step("Enter OTP for health care validation <otp> for healthID <healthID>",
+    async function (otp, healthID) {
+        await write(otp, into(textBox(above("Confirm"))));
+        var patientDetails = JSON.parse(_fileExtension.parseContent("./data/confirm/" + healthID + ".json"));
+        const token = process.env.receptionist
 
-    var confirm = _fileExtension.parseContent("./data/confirm/simple.txt")
-		.replace('<healthID>',patientDetails.healthID)
-		.replace('<fullName>',patientDetails.firstName+" "+patientDetails.lastName)
-		.replace('<gender>',patientDetails.gender)
-		.replace('<yearOfBirth>',patientDetails.yearOfBirth)
-		.replace('<district>',patientDetails.district)
-		.replace('<state>',patientDetails.state)
-		.replace('<mobileNumber>',patientDetails.mobileNumber)
-        .replace('<healthNumber>',patientDetails.healthNumber);
-    await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/v0.5/hip/auth/confirm",(request)=>{
-        request.respond({
-            method: 'POST',
-            port: '9052',
-            hostname: 'https://ndhm-dev.bahmni-covid19.in/',
-            path: '/v0.5/users/auth/on-init',
-            body: confirm,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token,
-                'content-length': confirm.length,
-                'X-HIP-ID': '10000005'
-            }
+        var patientFullName = patientDetails.firstName + " " + patientDetails.lastName
+        var confirm = _fileExtension.parseContent("./data/confirm/simple.txt")
+            .replace('<healthID>', patientDetails.healthID)
+            .replace('<fullName>', patientFullName)
+            .replace('<gender>', patientDetails.gender)
+            .replace('<yearOfBirth>', patientDetails.yearOfBirth)
+            .replace('<district>', patientDetails.district)
+            .replace('<state>', patientDetails.state)
+            .replace('<mobileNumber>', patientDetails.mobileNumber)
+            .replace('<healthNumber>', patientDetails.healthNumber);
+        await intercept("https://ndhm-dev.bahmni-covid19.in/ndhm/null/v0.5/hip/auth/confirm", (request) => {
+            request.respond({
+                method: 'POST',
+                port: '9052',
+                hostname: 'https://ndhm-dev.bahmni-covid19.in/',
+                path: '/v0.5/users/auth/on-init',
+                body: confirm,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token,
+                    'content-length': confirm.length,
+                    'X-HIP-ID': '10000005'
+                }
+            })
+        });
+        var existingPatientUrl = "https://ndhm-dev.bahmni-covid19.in/ndhm/null/existingPatients?patientName=" + patientDetails.firstName + "+" + patientDetails.lastName
+            + "&patientYearOfBirth=" + patientDetails.yearOfBirth + "&patientGender=" + patientDetails.gender;
+        await intercept(existingPatientUrl, (request) => {
+            var body1 = {
+                "error": { "code": "PATIENT_ID_NOT_FOUND", "message": "No patient found" }
+            };
+            var reqBodyOnFetchModes = JSON.stringify(body1);
+            request.respond({
+                method: 'GET',
+                port: '9052',
+                body: body1,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token,
+                    'content-length': reqBodyOnFetchModes.length
+                }
+            })
         })
+        await click(button("Confirm"))
+        await click(button("Update"))
     });
-    await click(button("Confirm"))
-});
