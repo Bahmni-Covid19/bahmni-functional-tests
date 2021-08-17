1b49de36feea95415151bdc61a7df8ae00388fc6
Soumya <sswaroop@thoughtworks.com>
2021-08-17 14:24:53 +0530

    Update programBasedVisit.js

--- tests/frontDesk/programBasedVisit.js
+++ tests/frontDesk/programBasedVisit.js
@@ -25,7 +25,7 @@ var path = require("path")
 var taikoHelper = require("../util/taikoHelper")
 
 step("Click Start Special OPD Visit", async function() {
-    await click(button(toRightOf('Start OPD Visit')))
+    await click(button(toRightOf('Start OPD Visit'), within($(".submit-btn-container"))));
     await click('Start Special OPD Visit',{waitForNavigation:true})
     await taikoHelper.repeatUntilNotFound($("#overlay"))
 });
